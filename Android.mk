5b08db2c6711166163740f28290aec7b23146dee
Marcos Marado <mmarado@cyngn.com>
2014-12-16 19:31:05 +0000

    CM12vification
    Conflicts:
    	Android.mk
    
    Change-Id: I910c471ede3876c7d2fc45e938dbd856f126e992

--- Android.mk
+++ Android.mk
@@ -1,10 +1,12 @@
 display-hals := include libgralloc libgenlock libcopybit liblight
 display-hals += libhwcomposer liboverlay libqdutils libhdmi libqservice
 display-hals += libmemtrack
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+display-hals += liblight
+endif
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
-endif
 endif
