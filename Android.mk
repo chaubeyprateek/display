4e6232ad5329a9319ae945a836f1352d959af4e1
Jake Whatley <jacob.whatley91@gmail.com>
2016-12-25 15:19:11 +0100

    display-caf-msm8916: guard makefile
    Change-Id: I8d193c5736dc70ed7cca290ee9fc13c61edbb968

--- Android.mk
+++ Android.mk
@@ -1,4 +1,5 @@
-display-hals := include libgralloc libgenlock libcopybit liblight
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf-msm8916)
+display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libhdmi libqservice
 display-hals += libmemtrack
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
@@ -10,3 +11,8 @@ else
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
+<<<<<<< HEAD
+=======
+endif
+endif #TARGET_QCOM_DISPLAY_VARIANT
+>>>>>>> f9f8d5332... display-caf-msm8916: guard makefile
