544346fbff3147e4d1be61762a2570cd5b609285
Rajshekar Eashwarappa <reashw@codeaurora.org>
2017-06-15 09:48:57 +0530

    display-caf: compilation fix
    Change-Id: Id4fe9d2f3bbfda9401b518c123df2b7f6a3dc6e6

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -7,8 +7,11 @@ LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes) \
                                  $(TOP)/external/skia/include/core \
-                                 $(TOP)/external/skia/include/images
-LOCAL_CLANG := false
+                                 $(TOP)/external/skia/include/images \
+                                 frameworks/native/libs/arect/include
+
+#LOCAL_CLANG := false
+
 ifeq ($(strip $(TARGET_USES_QCOM_DISPLAY_PP)),true)
 LOCAL_C_INCLUDES              += $(TARGET_OUT_HEADERS)/qdcm/inc \
                                  $(TARGET_OUT_HEADERS)/common/inc \
